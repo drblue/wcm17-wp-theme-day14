40187344901039b24eb1d2689dd15aca499cbbb4
Johan Nordström <johan@digitalvillage.se>
2018-04-25 11:05:17 +0200

    Lösningsförslag workshop dag 12.

---
+++ loop-templates/content-ms_student.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Post rendering content according to caller of get_template_part.
+ *
+ * @package understrap
+ */
+
+?>
+
+<article <?php post_class('col-md-4'); ?> id="post-<?php the_ID(); ?>">
+
+	<header class="entry-header">
+
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+		'</a></h2>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+
+			<div class="entry-meta">
+				<?php understrap_posted_on(); ?>
+			</div><!-- .entry-meta -->
+
+		<?php endif; ?>
+
+	</header><!-- .entry-header -->
+
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="entry-content">
+
+		Klasser:
+		<?php
+			the_terms(get_the_ID(), 'ms_class');
+		?>
+
+		<?php
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+			'after'  => '</div>',
+		) );
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
